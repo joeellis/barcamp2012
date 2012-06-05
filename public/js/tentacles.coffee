5270aa60073e56a35a4f758a95a0cb093868e88a
Ben Lavender <blavender@gmail.com>
2012-06-04 20:24:36 -0500

    add isVillainous() jquery function.

--- public/js/tentacles.coffee
+++ public/js/tentacles.coffee
@@ -11,26 +11,42 @@ $.fn.tentacleAttack = (opts) ->
       if pos == (@data('frames') - 1) * @data('offset') * -1
         @data 'withdrawing', true
         @data 'skipframe', 3
+        @trigger 'attacked'
     else
       pos += @data 'offset'
       if pos == 0
-        @removeData 'withdrawing'
-        @removeData 'animating'
-        clearInterval @data('attackInterval')
+        @trigger 'withdrawn'
     @css 'background-position', "#{position[0]} #{pos}px"
 
   if opts?
     # opts are sent: do setup
     @data 'offset', opts.offset
     @data 'frames', opts.frames
-  else if !@data('animating')?
-    # no opts sent: ATTACK!
-    @data 'animating', true
-    self = @
-    interval = setInterval ->
-      animateFrame.call self
-    , 40
-    @.data 'attackInterval', interval
+    @on 'withdrawn', =>
+      @removeData 'withdrawing'
+      @removeData 'animating'
+      clearInterval @data('attackInterval')
+
+  @on 'attack', =>
+    if !@data('animating')?
+      # no opts sent: ATTACK!
+      @data 'animating', true
+      self = @
+      interval = setInterval ->
+        animateFrame.call self
+      , 40
+      @trigger 'attacking'
+      @data 'attackInterval', interval
+
+$.fn.isVillainous = (who) ->
+  @.click =>
+    who.one 'attacked', =>
+      @explode()
+      @hide()
+    who.trigger 'attack'
+    setTimeout =>
+      @fadeIn()
+    , 5000
 
 $('.tentacle1').tentacleAttack frames: 5, offset: 600
 $('.tentacle2').tentacleAttack frames: 8, offset: 600
--- public/js/tentacles.js
+++ public/js/tentacles.js
@@ -1,7 +1,8 @@
 (function() {
 
   $.fn.tentacleAttack = function(opts) {
-    var animateFrame, interval, self;
+    var animateFrame,
+      _this = this;
     animateFrame = function() {
       var pos, position;
       position = this.css('background-position').split(" ");
@@ -14,28 +15,49 @@
         if (pos === (this.data('frames') - 1) * this.data('offset') * -1) {
           this.data('withdrawing', true);
           this.data('skipframe', 3);
+          this.trigger('attacked');
         }
       } else {
         pos += this.data('offset');
-        if (pos === 0) {
-          this.removeData('withdrawing');
-          this.removeData('animating');
-          clearInterval(this.data('attackInterval'));
-        }
+        if (pos === 0) this.trigger('withdrawn');
       }
       return this.css('background-position', "" + position[0] + " " + pos + "px");
     };
     if (opts != null) {
       this.data('offset', opts.offset);
-      return this.data('frames', opts.frames);
-    } else if (!(this.data('animating') != null)) {
-      this.data('animating', true);
-      self = this;
-      interval = setInterval(function() {
-        return animateFrame.call(self);
-      }, 40);
-      return this.data('attackInterval', interval);
+      this.data('frames', opts.frames);
+      this.on('withdrawn', function() {
+        _this.removeData('withdrawing');
+        _this.removeData('animating');
+        return clearInterval(_this.data('attackInterval'));
+      });
     }
+    return this.on('attack', function() {
+      var interval, self;
+      if (!(_this.data('animating') != null)) {
+        _this.data('animating', true);
+        self = _this;
+        interval = setInterval(function() {
+          return animateFrame.call(self);
+        }, 40);
+        _this.trigger('attacking');
+        return _this.data('attackInterval', interval);
+      }
+    });
+  };
+
+  $.fn.isVillainous = function(who) {
+    var _this = this;
+    return this.click(function() {
+      who.one('attacked', function() {
+        _this.explode();
+        return _this.hide();
+      });
+      who.trigger('attack');
+      return setTimeout(function() {
+        return _this.fadeIn();
+      }, 5000);
+    });
   };
 
   $('.tentacle1').tentacleAttack({
