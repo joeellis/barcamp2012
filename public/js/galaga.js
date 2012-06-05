a892da0f33b4395dc0285d45d84bdab43a03df07
Ben Lavender <blavender@gmail.com>
2012-06-05 12:31:28 -0500

    make helicopters explode when you click on them.

---
+++ public/js/galaga.js
@@ -0,0 +1,32 @@
+(function() {
+
+  $.fn.galagable = function(opts) {
+    var sound;
+    if (opts == null) opts = {};
+    if ($('audio#galaga').size() === 0) {
+      sound = $("<audio id='galaga' preload='auto'><source src='sounds/explosion.mp3' /><source src='sounds/explosion.ogg' /></audio>");
+      $('body').append(sound);
+    }
+    sound = $('audio#galaga');
+    this.each(function() {
+      var self,
+        _this = this;
+      self = $(this);
+      return self.click(function() {
+        var width;
+        width = self.width();
+        self.css('backgroundImage', 'none');
+        self.stop();
+        self.html("<img src='img/galaga_explosion.png' width=" + width + "/>");
+        sound.get(0).play();
+        return setTimeout(function() {
+          return self.remove();
+        }, 2000);
+      });
+    });
+    return this;
+  };
+
+  $('.helicopter').galagable();
+
+}).call(this);
